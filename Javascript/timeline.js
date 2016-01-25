8f66c465624f09b3459139b7b93582b3b3d93c1b
Isaac Patka <ipatka1@binghamton.edu>
2016-01-24 21:25:01 -0800

    weather timeline with formatted data points works. need to add tides and format container

--- Javascript/timeline.js
+++ Javascript/timeline.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
+
+// $("#weather").kinetic();
+
 checkTides();
 
-initChart();
+var chart = initChart();
 
-getWeather();
+WeatherToTimeline(chart);
 
 });
 
@@ -14,19 +17,16 @@ function checkTides() {
 }
 
 
-function getWeather() {
+function WeatherToTimeline(chart) {
 
 	$.ajax({
 			url: "https://api.forecast.io/forecast/"+forecastAPIKey+"/"+forecastLatLong,
 			type: "get",
 			dataType: "jsonp",
 			success: function(response) {
+				updateTimeline(chart, response);
 				console.log(response);
-			// self.weatherData = response;
 			
-			// self.updateCurrentWeather();
-			// self.updateGreeting();
-			/* self.updateupcomingWeather(); */
 			},
 			error: function() {
 				console.log("Couldn't grab weather");
@@ -34,9 +34,115 @@ function getWeather() {
 		});
 }
 
+function updateTimeline(chart, weather) {
+	// console.log(weather);
+	var temp = Math.round(weather.currently.temperature);
+	var icon = weather.currently.icon;
+	var currentprecip = weather.currently.precipIntensity;
+	var nexthour = weather.minutely.summary;
+	var nexthourshort = nexthour.replace("starting ","");
+	var mintilrain = nexthour.replace(/[^0-9]/g,'');
+	var hourlyweather = getHourlyWeatherData(weather);
+	setTimeline(chart, hourlyweather);
+}
+
+function getHourlyWeatherData(weather) {
+	upcomingWeather = [];
+	// console.log(Date.UTC(2016, 0, 21, 0, 30));
+	for (i = 0; i < 24; i+=2) {
+
+
+		console.log(weather.hourly.data[i].time*1000); // need the *1000
+		
+		var date = weather.hourly.data[i].time*1000;
+		// var date = new Date(weather.hourly.data[i].time*1000);
+		// console.log(date);
+		
+		/* TEMP */
+
+		var hourTemp = Math.round(weather.hourly.data[i].temperature);
+
+		/* SYMBOL */
+
+		var hourSymbol = weather.hourly.data[i].icon;
+
+		/* RAIN SYMBOL CHECK */
+		
+		if (hourSymbol == "rain") {
+			var rainIntensity = weather.hourly.data[i].precipIntensity;
+			// console.log(rainIntensity);
+			switch (true) {
+				case (rainIntensity <= 0.015): // drizzle 0.002
+					hourSymbol += "-drizzle";
+					break;
+				case (rainIntensity > 0.015 || rainIntensity <= 0.08): // light 0.017
+					hourSymbol += "-light";
+					break;
+				case (rainIntensity > 0.08 || rainIntensity <= 0.3): // med 0.1
+					hourSymbol += "-med";
+					break;
+				case (rainIntensity > 0.3): // heavy 0.4
+					hourSymbol += "-heavy";
+					break;
+			};
+		}
+
+		/* TEMP FONT COLOR SET */
+
+		var hourFontColor;
+
+		switch (hourSymbol) {
+			case("snow"):
+			case("rain-drizzle"):
+			case("rain-light"):
+			case("rain-med"):
+			case("rain-heavy"):
+				hourFontColor = "#0071ba";
+				break
+			case("clear-night"):
+			case("cloudy"):
+			case("partly-cloudy-night"):
+				hourFontColor = "#808080";
+				break
+			case("fog"):
+			case("wind"):
+				hourFontColor = "#ab44f8";
+				break
+			case("clear-day"):
+			case("partly-cloudy-day"):
+				hourFontColor = "#ffc600";
+				break
+		}
+
+		// var formattedWeather = $.parseJSON('{ "y" : '+hourTemp+', "marker": { "symbol": "url(images/icons/'+hourSymbol+'.png)", "width": 45, "height": 45 }, "dataLabels": { "color": "'+hourFontColor+'" } }');
+		var formattedWeather = hourTemp;
+
+
+		dataPoint = [
+			date,
+			hourTemp
+		];
+
+		// var formattedDataPoint = "";
+		var formattedDataPoint = $.parseJSON('{ "x" : '+date+', "y" : '+hourTemp+', "marker": { "symbol": "url(images/icons/'+hourSymbol+'.png)", "width": 45, "height": 45 }, "dataLabels": { "color": "'+hourFontColor+'" } }');
+		// Push into array
+		
+		upcomingWeather.push(formattedDataPoint);
+
+	}
+	// console.log(upcomingWeather);
+	return upcomingWeather;
+
+}
+
+function setTimeline(chart, data) {
+	chart.series[0].setData(data);
+}
+
 function initChart() {
-	$('#weather').highcharts({
+	var chart = new Highcharts.Chart({
         chart: {
+            renderTo: 'weather',
             type: 'spline'
         },
         title: {
@@ -114,33 +220,16 @@ function initChart() {
             color: '#dfdfdf',
             dataLabels:{
             enabled: true
-            },
-            // Define the data points. All series have a dummy year
-            // of 1970/71 in order to be compared on the same x axis. Note
-            // that in JavaScript, months start at 0 for January, 1 for February etc.
-            data: [
-                [Date.UTC(1970, 9, 21, 0, 30), 0],
-                [Date.UTC(1970, 9, 21, 1, 30), 0.28],
-                [Date.UTC(1970, 9, 21, 2, 30), 0.25],
-                [Date.UTC(1970, 9, 21, 3, 0), 0.2],
-                [Date.UTC(1970, 9, 21, 3, 30), 0.28]
-            ]
+            }
         },{
         		yAxis:1,
             name: 'Tides',
             marker: {
-                symbol: 'url(http://thumb7.shutterstock.com/display_pic_with_logo/356785/262919369/stock-vector-edd-single-flat-icon-on-white-background-vector-illustration-262919369.jpg)',
                 width: 16,
                 height: 16
             },
-            data: [
-            		[Date.UTC(1970, 9, 21, 0, 30), 0],
-                [Date.UTC(1970, 9, 21, 1, 30), 0],
-                [Date.UTC(1970, 9, 21, 2, 30), 0],
-                [Date.UTC(1970, 9, 21, 3, 0), 0],
-                [Date.UTC(1970, 9, 21, 3, 30), 0]
-            ],
             lineWidth: 0
         }]
     });
+return chart;
 }
